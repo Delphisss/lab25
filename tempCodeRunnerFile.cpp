2c42fc448e998041fd7da02ca4e246aa779acff5
D-Delphis <kong_led_@hotmail.com>
2023-03-10 15:30:55 +0700

    lab25 งงในงง

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,23 @@
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
+
+	myList.insert(29,2);
+	myList.insert(33,3);
+	myList.insert(77,5);
+	myList.insert(69,0);
+	myList.show();
+	cout << "\n"; 
+
+	myList.remove(2);
+	myList.show();
+	cout << "\n"; 
+	myList.remove(4);
+	myList.show();
+	cout << "\n";
+	myList.remove(0);
+	myList.show();
+	cout << "\n";
